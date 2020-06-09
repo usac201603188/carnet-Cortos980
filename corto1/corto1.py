fa2a06647760c2348b5f122ad930ab014b02c965
usac201603188 <3001784840101@ingenieria.usac.edu.gt>
2020-06-09 12:46:40 -0600

    comentado

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -11,17 +11,17 @@ def impares(N):
 
 archivo=open("collatz.txt","w")
 
-for i in range(2,189,1):
+for i in range(2,189,1):    #Todo dentro de una lista para que ejecute cada secuencia en una linea diferente
     lista=[i]
-    d=i
+    d=i                      #para no afectar la i que se está leyendo agrego una variable que servirá despues
     while lista[-1]!=1:
-        if(d%2==0):
+        if(d%2==0):         #Evaluacion si son pares o impares
             r=pares(d)
         else:
             r=impares(d)
-        d=r
+        d=r                 #igualo mi variable al resultado obtenido 
         lista.append(int(r))
-    archivo.write(str(lista)+"\n")    
+    archivo.write(str(lista)+"\n")    #Escritura del archivo 
 
 
 archivo.close()
